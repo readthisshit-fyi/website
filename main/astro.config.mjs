8dccf69e8d6b8e739735bb679b5e666ee863fa67
Mark <github@yesitsme.dev>
2026-07-09 20:07:17 +0300

    I've changed the font, added a translation system, etc. etc. etc...

--- main/astro.config.mjs
+++ main/astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig, fontProviders } from "astro/config";
 
 import vue from "@astrojs/vue";
 import svgLoader from "vite-svg-loader";
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
+import node from "@astrojs/node";
+import { locales } from "./src/paraglide/runtime.js";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: node({ mode: "standalone" }),
+
   server: {
     host: "0.0.0.0",
     port: 4321,
@@ -15,10 +21,47 @@ export default defineConfig({
     css: {
       transformer: "lightningcss",
     },
-    plugins: [svgLoader()],
+    plugins: [
+      svgLoader(),
+      paraglideVitePlugin({
+        project: "./project.inlang",
+        outdir: "./src/paraglide",
+        strategy: ["url", "cookie", "baseLocale"],
+        cookieName: "PARAGLIDE_LOCALE",
+        urlPatterns: [
+          {
+            pattern: "/",
+            localized: locales.map((locale) => [locale, `/${locale}`]),
+          },
+          {
+            pattern: "/:path(.*)?",
+            localized: locales.map((locale) => [locale, `/${locale}/:path(.*)?`]),
+          },
+        ],
+      }),
+    ],
   },
 
   fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Fixel",
+      cssVariable: "--fixel",
+      options: {
+        variants: [
+          {
+            weight: "100 900",
+            style: "normal",
+            src: ["./src/assets/Fixel.ttf"],
+          },
+          {
+            weight: "100 900",
+            style: "italic",
+            src: ["./src/assets/Fixel-Italic.ttf"],
+          },
+        ],
+      },
+    },
     {
       provider: fontProviders.local(),
       name: "Work Sans",
